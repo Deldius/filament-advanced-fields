0cef00d91019b49fdcaf85bfe34b045198628491
Trung <trungk10.uel@gmail.com>
2025-07-20 14:54:45 +0700

    Clean up repo

---
+++ resources/lang/en/advanced-fields.php
@@ -0,0 +1,6 @@
+<?php
+
+// translations for Deldius/AdvancedFields
+return [
+    //
+];
---
+++ src/AdvancedFieldsServiceProvider.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Deldius\AdvancedFields;
+
+use Filament\Support\Assets\AlpineComponent;
+use Filament\Support\Assets\Asset;
+use Filament\Support\Assets\Css;
+use Filament\Support\Assets\Js;
+use Filament\Support\Facades\FilamentAsset;
+use Filament\Support\Facades\FilamentIcon;
+use Illuminate\Filesystem\Filesystem;
+use Livewire\Features\SupportTesting\Testable;
+use Spatie\LaravelPackageTools\Commands\InstallCommand;
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Deldius\AdvancedFields\Commands\AdvancedFieldsCommand;
+use Deldius\AdvancedFields\Testing\TestsAdvancedFields;
+
+class AdvancedFieldsServiceProvider extends PackageServiceProvider
+{
+    public static string $name = 'filament-advanced-fields';
+
+    public static string $viewNamespace = 'filament-advanced-fields';
+
+    public function configurePackage(Package $package): void
+    {
+        /*
+         * This class is a Package Service Provider
+         *
+         * More info: https://github.com/spatie/laravel-package-tools
+         */
+        $package->name(static::$name)
+            ->hasViews(static::$viewNamespace)
+            ->hasConfigFile()
+            ->hasTranslations();
+    }
+
+    public function packageRegistered(): void {}
+
+    public function packageBooted(): void
+    {
+        // FilamentAsset::register(
+        //     [
+        //         Css::make(
+        //             'filament-advanced-fields',
+        //             __DIR__ . '/../resources/dist/filament-advanced-fields.css'
+        //         )
+        //         // ->loadedOnRequest()
+        //         ,
+        //     ],
+        //     $this->getAssetPackageName()
+        // );
+    }
+
+    protected function getAssetPackageName(): ?string
+    {
+        return 'deldius/filament-advanced-fields';
+    }
+}
